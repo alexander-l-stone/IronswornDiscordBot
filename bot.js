5fd009e30ffa5f63e3dbd6cd4b70b848d8d1077b
Alexander Stone <aloecend@gmail.com>
2019-01-25 12:02:53 -0800

    Changed bot to use discord.js instead of discord.io

--- bot.js
+++ bot.js
@@ -1,39 +1,14 @@
-var Discord = require('discord.io');
-var logger = require('winston');
+const Discord = require('discord.js');
 var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-  colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-  token: auth.token,
-  autorun: true
-});
-bot.on('ready', function (evt) {
-  logger.info('Connected');
-  logger.info('Logged in as: ');
-  logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-  // Our bot needs to know if it will execute a command
-  // It will listen for messages that will start with `!`
-  if (message.substring(0, 1) == '!') {
-    var args = message.substring(1).split(' ');
-    var cmd = args[0];
+const client = new Discord.Client();
 
-    args = args.splice(1);
-    switch (cmd) {
-      // !ping
-      case 'ping':
-        bot.sendMessage({
-          to: channelID,
-          message: 'Pong!'
-        });
-        break;
-        // Just add any case commands if you want to..
-    }
-  }
+client.on('ready', () => {
+  console.log("Connected as " + client.user.tag);
 });
+
+// Get your bot's secret token from:
+// https://discordapp.com/developers/applications/
+// Click on your application -> Bot -> Token -> "Click to Reveal Token"
+bot_secret_token = auth.token;
+
+client.login(bot_secret_token);
