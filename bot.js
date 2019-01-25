6998d5c05b95f0c7a19904b3e1c4c19e9d3fc381
Alexander Stone <aloecend@gmail.com>
2019-01-25 12:32:30 -0800

    Added basic help command.

--- bot.js
+++ bot.js
@@ -3,12 +3,52 @@ var auth = require('./auth.json');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log("Connected as " + client.user.tag);
+  // List servers the bot is connected to
+  console.log("Servers:")
+  client.guilds.forEach((guild) => {
+      console.log(" - " + guild.name);
+
+      // List all channels
+      guild.channels.forEach((channel) => {
+          console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`);
+      });
+  });
+});
+bot_secret_token = auth.token;
+
+client.on('message', (receivedMessage) => {
+  if (receivedMessage.author == client.user)
+  {
+    return;
+  }
+  
+  if (receivedMessage.content.startsWith("!"))
+  {
+    parseCommand(receivedMessage);
+  }
+  else if (receivedMessage.content == "?")
+  {
+    parseHelp(receivedMessage);
+  }
 });
 
-// Get your bot's secret token from:
-// https://discordapp.com/developers/applications/
-// Click on your application -> Bot -> Token -> "Click to Reveal Token"
-bot_secret_token = auth.token;
+function parseCommand(receivedMessage){
+  return;
+}
+
+function parseHelp(receivedMessage){
+  let fullMessage = receivedMessage.content.substr(1);
+  if (fullMessage === "")
+  {
+    helpCommand("", receivedMessage);
+  }
+  return;
+}
+
+function helpCommand(args, receivedMessage) {
+  if (args === "") {
+    receivedMessage.channel.send("This is a bot for playing the tabletop RPG Ironsworn on your server. Type ? and then a command to learn more. The commands are: ")
+  }
+}
 
 client.login(bot_secret_token);
